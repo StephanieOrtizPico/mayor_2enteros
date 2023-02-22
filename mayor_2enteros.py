fcd68d3de9a9156dd0d0dcf550579e80b25dfcae
StephanieOrtizPico <nicolleortiz@colegioguanenta.edu.co>
2023-02-22 09:13:59 -0500

    Se modificó el programa

--- mayor_2enteros.py
+++ mayor_2enteros.py
@@ -5,8 +5,13 @@ x = int(input ("Digite el primer número: "))
 y = int(input ("Digite el segundo número: "))
 
 # processing 
-if x>y: 
-    print("El número mayor es: " +str(x))
-
-else: 
-    print("El número mayor es: "+str(y))
+if x == y:
+    # output
+    print("Los números son iguales...")
+else:
+    if x > y:
+        mayor = x
+    else:
+        mayor = y
+# output
+print("El número mayor entre " +str(x)+ " y " +str(y)+ " es " +str(mayor))
